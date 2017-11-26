5a02fb25655b2417626039a596af9363e0e88812
Kevin Meyer <//>
2017-11-26 17:11:47 +0100

    Comments everywhere

--- src/com/company/ConsoleWriter.java
+++ src/com/company/ConsoleWriter.java
@@ -4,6 +4,10 @@ public class ConsoleWriter {
 
     private ConsoleWriter(){}
 
+    /**
+     * Prints a message in the console.
+     * @param msg
+     */
     public static void writeToConsole(String msg) {
         System.out.println(msg);
     }
--- src/com/company/IGameLogic.java
+++ src/com/company/IGameLogic.java
@@ -1,5 +1,8 @@
 package com.company;
 
 public interface IGameLogic {
+    /**
+     * Starts the game.
+     */
     void startGame();
 }
